dd20444b86108f37558919e2507cc788a409c736
S. M. JAHANGIR <smjxpro@gmail.com>
2021-12-31 00:10:07 +0600

    fix registration issue

--- Onath.Web/Pages/Account/Register.cshtml.cs
+++ Onath.Web/Pages/Account/Register.cshtml.cs
@@ -45,13 +45,13 @@ public class Register : PageModel
             {
                 _logger.LogInformation("User created a new account with password.");
 
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new {area = "Identity", userId = user.Id, code = code},
-                    protocol: Request.Scheme);
+                // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                // var callbackUrl = Url.Page(
+                //     "/Account/ConfirmEmail",
+                //     pageHandler: null,
+                //     values: new {area = "Identity", userId = user.Id, code = code},
+                //     protocol: Request.Scheme);
 
                 // await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                 //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
